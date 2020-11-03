ac1200e139859ac3e47ad1e0507a48320a38e73f
Noor-Shinh <harnoors966@deltalearns.ca>
2020-11-03 13:06:08 -0800

    changes

--- task2.py
+++ task2.py
@@ -14,4 +14,66 @@ between + and -.
 """
 import tkinter as tk 
 from tkinter import *
+import math 
+win=tk.Tk()
+win.title("Factoring Simple Trinomials")
+eoutput=StringVar()
+variable1=StringVar()
+variable2=StringVar()
 
+def factor():
+    a=1
+    b=a1.get()
+    c=a2.get()
+    d=variable1.get()
+    e=variable2.get()
+    if d=="+":
+        b=b*1
+    elif x1=="-":
+        b=b*-1
+    if e=="+":
+        c=c*1
+    elif x2=="-":
+        c=c*-1
+    stepx1=(b*-1)
+    stepx2=math.pow(b,2)-(4*a*c)
+    stepx2=math.sqrt(stepx2)
+    stepx3=(stepx1)+(stepx2) 
+    stepx4=(stepx3)/(2*a)
+    stepy1=(b*-1)
+    stepy2=math.pow(b,2)-(4*a*c)
+    stepy2=math.sqrt(stepy2)
+    stepy3=(stepy1)-(stepy2) 
+    stepy4=(stepy3)/(2*a)
+    stepx4=round(stepx4,2)
+    stepy4=round(stepy4,2)
+    if stepx4>0:
+        stepx5=("x"+str(stepx4*-1))
+    elif stepx4<0:
+        stepx5=("x+"+str(stepx4*-1))
+    if stepy4>0:
+        stepy5=("x"+str(stepy4*-1))
+    elif stepy4<0:
+        stepy5=("x+"+str(stepy4*-1))
+    numlist=[stepy5,stepx5]
+    eoutput.set(numlist)
+
+l1=Label(win,text="bx value")
+l2=Label(win,text="c value")
+l3=Label(win,text="ax value is always equal to 1")
+a1=Entry(win,width=40)
+a2=Entry(win,width=40)
+x1=OptionMenu(win,variable1,"+","-")
+x2=OptionMenu(win,variable2,"+","-")
+b1=Button(win,text="Factor",command=factor)
+f1=Label(win,textvariable=eoutput,width=80)
+
+l1.grid(row=1,column=1)
+a1.grid(row=1,column=3)
+l2.grid(row=2,column=1)
+a2.grid(row=2,column=3)
+x1.grid(row=1,column=2)
+x2.grid(row=2,column=2)
+b1.grid(row=3,column=1)
+f1.grid(row=4,column=1)
+win.mainloop()
